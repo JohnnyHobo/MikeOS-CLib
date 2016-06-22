3fb981dcf1acd96c7e344e8f43c95a94ea5cef74
ZeroKelvinKeyboard <zerokelvinkeyboard@gmail.com>
2016-06-23 02:51:56 +0930

    Added header file for TextIO library.

---
+++ include/textio.h
@@ -0,0 +1,26 @@
+#ifndef MOSCLIB_HAS_TEXTIO
+#define MOSCLIB_HAS_TEXTIO
+
+void textio_init();
+void textio_write_char(char ch);
+char textio_read_char();
+int textio_draw_char(int colour, char ch, int y, int x);
+void textio_draw_hline(int colour, char ch, int length, int start_y, int start_x);
+void textio_draw_vline(int colour, char ch, int length, int start_y, int start_x);
+void textio_draw_block(int colour, char ch, int start_y, int start_x, int finish_x, int finish_y);
+void textio_grab_area(int start_y, int start_x, int finish_y, int finish_x, char *buffer);
+void textio_restore_area(int start_y, int start_x, int finish_y, int finish_x, char *buffer);
+void textio_set_text_colour(int colour);
+void textio_set_output_page(int page);
+void textio_set_visible_page(int page);
+void textio_print_string(char *str);
+void textio_reverse_cursor();
+void textio_advance_cursor();
+void textio_newline();
+void textio_set_cursor(int row, int column);
+void textio_get_cursor(int *row, int *column);
+void textio_scroll_down(int lines);
+void textio_clear_screen();
+void textio_set_screen_limits(int row, int column);
+
+#endif
